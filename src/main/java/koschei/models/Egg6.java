63b8057b08b534914a46eff590a0bac68efbe1e2
kamil <kh8@ramler.ru>
2023-10-12 20:06:18 +0300

    final

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,21 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+    private final Deth8 deth8;
+
+    @Autowired
+
+    public Egg6( Deth8 deth8) {
+        this.deth8 = deth8;
+    }
+
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + deth8.toString();
     }
 }
